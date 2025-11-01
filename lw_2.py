ddfa585dc73230abe3b692f6f2ffb3cfa15efe0a
Arina Lotoshvili <arinalotosvili@gmail.com>
2025-11-01 16:57:28 +0300

    Update lw_2.py

--- lw_2.py
+++ lw_2.py
@@ -1,6 +1,7 @@
 import csv
 import random
 from os import write
+import xml.dom.minidom as mdom
 
 'task 1'
 
@@ -56,16 +57,58 @@ from os import write
 #     f.write('\n')
 # f.close()
 
-answ = []
-with open('books.csv') as f:
-    s = list(csv.DictReader(f, delimiter=';'))
-    for _ in range(20):
-        row = random.choice(s)
-        answ.append(row)
-f = open('lw_2_task3.txt', 'w', encoding='utf-8')
-for index, el in enumerate(answ, start=1):
-    f.write(f'{index} {el['Автор']}. {el['Название']} - {el['Дата поступления'].split()[0].split('.')[-1]}')
-    f.write('\n')
-f.close()
+# answ = []
+# with open('books.csv') as f:
+#     s = list(csv.DictReader(f, delimiter=';'))
+#     for _ in range(20):
+#         row = random.choice(s)
+#         answ.append(row)
+# f = open('lw_2_task3.txt', 'w', encoding='utf-8')
+# for index, el in enumerate(answ, start=1):
+#     f.write(f'{index} {el['Автор']}. {el['Название']} - {el['Дата поступления'].split()[0].split('.')[-1]}')
+#     f.write('\n')
+# f.close()
 
 'task 4'
+
+# import xml.dom.minidom as minidom
+#
+# xml_file = open('currency.xml')
+# xml_data = xml_file.read()
+#
+# dom  = minidom.parseString(xml_data) # dom = document object module
+# dom.normalize()
+#
+# elements1 = dom.getElementsByTagName('Name')
+# elements2= dom.getElementsByTagName('CharCode')
+# dict = {}
+# sp1 = []
+# sp2 = []
+#
+# def lis(elem, sp):
+#     for node in elem:
+#         for child in node.childNodes:
+#                 if child.nodeType == 3:
+#                         sp.append(child.data)
+#     return sp
+#
+#
+# sp1 = lis(elements1, sp1)
+# sp2 = lis(elements2, sp2)
+#
+# for i in range(len(sp1)):
+#     dict[sp1[i]] = sp2[i]
+#
+# print(dict)
+
+'extra'
+
+# with open('books.csv') as f:
+#     s = csv.reader(f, delimiter=';')
+#     head = next(s)
+#     s = list(s)
+#
+# s2 = sorted(s, key=lambda x: int(x[8]), reverse=True)
+# print(head)
+# for i in range(20):
+#     print(s2[i][1], s2[i][8])
